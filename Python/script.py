dc229ed5298b20c9d46210d5ca2740056db0aeba
LDDSY <100818596+Moveable-Feast@users.noreply.github.com>
2026-01-22 02:55:52 +0000

    new file:   "Python/LeetCode/\347\203\255\351\242\230100/4_\345\255\220\344\270\262/template.ipynb" 	modified:   Python/script.py

--- Python/script.py
+++ Python/script.py
@@ -1,6 +1,3 @@
-nums = [1,2,3]
-nums_old = nums
-nums_new = nums.copy()
-nums_new = [4,5,6]
-nums = nums_new.copy()
-print(nums, nums_new, nums_old)
+nums = [1, 2, 3, 4, 5]
+nums = nums - [1]
+print(nums)
